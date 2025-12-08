9ff713bf9d7cf0be07dec2be54556814a6930e4f
ViaYBA <viaysabelle@gmail.com>
2025-12-09 00:57:02 +0800

    Added Insert Queries

--- viewClientExp.php
+++ viewClientExp.php
@@ -13,7 +13,7 @@ if ($result && $result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         $name = htmlspecialchars($row['name']);
         $rating = (int)$row['rating'];
-        $review_text = htmlspecialchars($row['feedback']);
+        $feedback = htmlspecialchars($row['feedback']);
         
         //Stars Rating
         $stars_html = '';
